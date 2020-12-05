e988a58acea817249b7eb89809bb922ecdcd76a8
Martin Ryba <martin.ryba01@gmail.com>
2020-12-05 01:17:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Outputs the current weather conditions
+
+uses [ElementTree](https://docs.python.org/3/library/xml.etree.elementtree.html)
+
+
+# To-do
+- [ ] If error status code, use stored xml file
+- [ ] Daemon mode, that executes at exact hours
+- [ ] Option to change the city
+- [ ] Weather icons
---
+++ weather.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+import requests
+import xml.etree.ElementTree as ET
+from datetime import datetime
+from dateutil import parser
+
+
+url = "https://www.yr.no/place/Sweden/%C3%96sterg%C3%B6tland/Link%C3%B6ping/"
+response = requests.get(url)
+
+# redirected from search
+if response.url != url:
+    url = response.url
+    #url = url.replace("/sted/", "/place/")
+response = requests.get(url + "forecast_hour_by_hour.xml")
+
+if response.status_code != 200:
+    # print("server error")
+    exit(1)
+# print(response.text)
+# print(response.url)
+root = ET.fromstring(response.text)
+
+items = root.iter("time")
+
+cur_time = next(items)
+#symbol = cur_time.find("symbol").get("name")
+temp = cur_time.find("temperature").get("value")
+#rain = cur_time.find("precipitation").get("value")
+symbol = ""
+rain = ""
+
+print(temp + "° ", end='')
+
+#rain_start = None
+#if float(rain) > 0:
+#    symbol = symbol + "("+rain+")"
+#    rain_start = cur_time
+#else:  # find start of rain
+#    for item in items:
+#        if float(item.find("precipitation").get("value")) > 1.0:
+#            rain_start = item
+#            break
+#
+#if rain_start is not None:
+#    rain_end = cur_time
+#    for item in items:
+#        if float(item.find("precipitation").get("value")) > 1.0:
+#            rain_end = item
+#        else:
+#            break
+#
+#    if rain_start is not cur_time or rain_end is not cur_time:
+#        print(" ", end='')
+#        
+#    date_start = parser.parse(rain_start.get("from"))
+#
+#    if rain_start is not cur_time:
+#        print(date_start.hour, "-", sep='', end='')
+#    elif rain_end is not cur_time:
+#        print("-", end='')
+#
+#    if rain_end is not None and rain_end is not cur_time:
+#        date_end = parser.parse(rain_end.get("to"))
+#        print(date_end.hour, end='')
+#    elif rain_start is not None:
+#        date_end = parser.parse(rain_start.get("to"))
+#        print(date_end.hour, end='')
+#
+#    day_diff = abs(date_start.day - datetime.now().day)
+#    if day_diff > 0:
+#        print("(", day_diff, ")", sep='', end='')
+#
+#
+print()
